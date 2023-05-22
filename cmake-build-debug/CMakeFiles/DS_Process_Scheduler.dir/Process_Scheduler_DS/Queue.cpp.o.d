94098d4cb02ecfaa48348f1e6d4d7e42f3e36024
moghaith <42257515+moghaith@users.noreply.github.com>
2023-05-22 12:57:22 +0300

    misc

--- cmake-build-debug/CMakeFiles/DS_Process_Scheduler.dir/Process_Scheduler_DS/Queue.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/DS_Process_Scheduler.dir/Process_Scheduler_DS/Queue.cpp.o: \
-  /Users/mohamedghaith/Documents/DS-Process-Scheduler/Process_Scheduler_DS/Queue.cpp
--- cmake-build-debug/CMakeFiles/DS_Process_Scheduler.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for DS_Process_Scheduler.
-# This may be replaced when dependencies are built.
